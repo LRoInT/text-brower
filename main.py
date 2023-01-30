f466d23372eca3e3174f40c67e00a321ad220c51
ETOemperor <lumaochen@yeah.net>
2023-01-30 19:24:54 +0800

    第一次更新

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import sys
 from lib import *
-from account import *
 from pyhtml import rqurl
 over=False
 about_user=[{"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 Edg/109.0.1518.70"},{"cookies":""}]
